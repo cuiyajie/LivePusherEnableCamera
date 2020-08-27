f0ce96fc1491660556e9896ded2976c5f88695a0
Cuiyajie <cuiyajie_dlut@hotmail.com>
2020-08-27 12:04:55 +0800

    New issue

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,12 +1,20 @@
 import Nerv, { Component } from 'nervjs'
-import { View, Text, LivePusher } from '@tarojs/components'
+import { View, Button, LivePusher } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 import './index.scss'
 
 export default class Index extends Component {
 
-  componentWillMount () { }
+  state = {
+    animation: ""
+  }
 
-  componentDidMount () { }
+  componentWillMount () {
+  }
+
+  componentDidMount () {
+    this.animationCtx = Taro.createAnimation()
+  }
 
   componentWillUnmount () { }
 
@@ -14,11 +22,20 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  animationCtx = null
+
+  onClick() {
+    this.setState({
+      animation: this.animationCtx.scale(3, 3).translate(70, 20).step().export()
+    })
+  }
+
   render () {
+    console.log(this.state.animation)
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
-        <LivePusher style={{width: '100vw', height: '100vh'}} enableCamera={false}></LivePusher>
+        <LivePusher style={{width: '50px', height: '50px'}} animation={this.state.animation}></LivePusher>
+        <Button onClick={this.onClick.bind(this)}>Click me</Button>
       </View>
     )
   }
